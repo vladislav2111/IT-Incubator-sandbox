8773ef193ab5797c5b4fd85e4b759cf3792ff8b4
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-28 13:17:14 +0300

    likes

--- 5kyu/parsedomain.js
+++ 5kyu/parsedomain.js
@@ -1,6 +1,7 @@
 function domainName(url){
-	/* return url.match(/(?<=.+:\/\/).+(?=\..)|(?<=www\.).+(?=\..)/)[0].split('.')[0]; */
-	return url.replace('http://','').replace('https://','').replace('www.','').split('.')[0];
+/* 	return url.match(/(?<=.+:\/\/).+(?=\.)|(?<=www\.).+(?=\..)/)[0].split('.')[0]; */
+	/* return url.replace('http://','').replace('https://','').replace('www.','').split('.')[0]; */
+	return url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
  }
 
 console.log(domainName('http://google.co.jp'));
