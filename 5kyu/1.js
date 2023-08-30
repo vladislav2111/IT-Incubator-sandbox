542325bb9924e0eee2e5570ea7602d97cab1e4e6
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-30 12:04:54 +0300

    bouncer

--- 5kyu/1.js
+++ 5kyu/1.js
@@ -1,5 +1,60 @@
-function findElement(arr, func) {
-	return arr.find(func);
- }
+function calc(expr) {
+	let expression = expr.replace(/\s+/g, "").split("");
 
-console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+	function peek() {
+		return expression[0] || "";
+	}
+
+	function get() {
+		return expression.shift();
+	}
+
+	function number() {
+		let res = get();
+		while ((peek() >= "0" && peek() <= "9") || peek() == ".") {
+			res += get();
+		}
+		return parseFloat(res);
+	}
+
+	function fact() {
+		if (peek() >= "0" && peek() <= "9") {
+			return number();
+		} else if (peek() == "(") {
+			get(); // '('
+			let res = final();
+			get(); // ')'
+			return res;
+		} else if (peek() == "-") {
+			get();
+			return -fact();
+		}
+		return 0; 
+	}
+
+	function term() {
+		let res = fact();
+		while (peek() == "*" || peek() == "/") {
+			if (get() == "*") {
+				res *= fact();
+			} else {
+				res /= fact();
+			}
+		}
+		return res;
+	}
+
+	function final() {
+		let res = term();
+		while (peek() == "+" || peek() == "-") {
+			if (get() == "+") {
+				res += term();
+			} else {
+				res -= term();
+			}
+		}
+		return res;
+	}
+
+	return final();
+}
