958afb421f393720a213ec6e6c043d9670545051
Vladislav Kudryavtsev <vladislavk2111@gmail.com>
2023-08-31 19:18:49 +0300

    mess

--- 5kyu/1.js
+++ 5kyu/1.js
@@ -1,6 +1,18 @@
-function correct(string)
-{
-	return string.replaceAll('5', 'S').replaceAll('0', 'O').replaceAll('1', 'I')
-}
+function expandedForm(num) {
+	let str = num+""
+	let length = str.length;
+	let res = "";
+	let sq = length-1;
+	for (let i=0; i<length;i++){
+	  if(str[i]!=0){
+		 res+=`${(10**sq)*+str[i]} + `;
+		 sq--;
+	  } else {
+		sq--;
+	  }
+	}
+	res = res.slice(0, res.length-3)
+	return res ;
+ }
 
-console.log(correct("L0ND0N"));
+ console.log(expandedForm(70304));
